b51df9e6c717a9cad76f6ad3bb2d791f93cda4e5
Matt Lauber <github@mklauber.com>
2015-07-12 15:17:47 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'pyService Skeleton',
+    'author': 'Matthew Lauber',
+    'url': 'https://github.com/mklauber/pyService',
+    'download_url': 'git@github.com:mklauber/pyService.git',
+    'author_email': 'github.com@mklauber.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['pyService'],
+    'scripts': [],
+    'name': 'pyService'
+}
+
+setup(**config)
